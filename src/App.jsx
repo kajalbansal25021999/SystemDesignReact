a598ab69e9f854a901f1146f12ce6372fcf05488
kajal <kajalbansal25021999@gmail.com>
2024-08-08 13:43:16 +0530

    creating and passing props in styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import SplitScreen from "./components/SplitScreen";
+
+const LeftSideComp = ({ title }) => {
+  return <h2 style={{ backgroundColor: "crimson" }}>{title}</h2>;
+};
+
+const RightSideComp = ({ title }) => {
+  return <h2 style={{ backgroundColor: "brown" }}>{title}</h2>;
+};
 
 const App = () => {
-  return <div>react design patterns and system design</div>;
+  return (
+    <>
+      <SplitScreen leftWidth={1} rightWidth={3}>
+        <LeftSideComp title={"Left"} />
+        <RightSideComp title={"Right"} />
+      </SplitScreen>
+    </>
+  );
 };
 
 export default App;
